2349c4fefe3dbdaadfb9d66e0eb20357157bba65
umit.exe <115349562+Umit-exe@users.noreply.github.com>
2022-10-18 14:45:04 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Ornek2020
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenGlControl = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // OpenGlControl
+            // 
+            this.OpenGlControl.AccumBits = ((byte)(0));
+            this.OpenGlControl.AutoCheckErrors = false;
+            this.OpenGlControl.AutoFinish = false;
+            this.OpenGlControl.AutoMakeCurrent = true;
+            this.OpenGlControl.AutoSwapBuffers = true;
+            this.OpenGlControl.BackColor = System.Drawing.Color.Black;
+            this.OpenGlControl.ColorBits = ((byte)(32));
+            this.OpenGlControl.DepthBits = ((byte)(16));
+            this.OpenGlControl.Location = new System.Drawing.Point(-1, -2);
+            this.OpenGlControl.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.OpenGlControl.Name = "OpenGlControl";
+            this.OpenGlControl.Size = new System.Drawing.Size(1102, 686);
+            this.OpenGlControl.StencilBits = ((byte)(0));
+            this.OpenGlControl.TabIndex = 0;
+            this.OpenGlControl.Paint += new System.Windows.Forms.PaintEventHandler(this.MyPaint);
+            this.OpenGlControl.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MyKeys);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Döndür";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(36, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "BEN10 OMNİTRİX";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1097, 683);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.OpenGlControl);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl OpenGlControl;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tao.OpenGl;
+using Tao.Platform.Windows;
+
+namespace Ornek2020
+{
+    public partial class Form1 : Form
+    {
+        float alfa = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            OpenGlControl.InitializeContexts();
+            // OpenGL ilk işlemler
+            Gl.glClearColor(0.1f, 0.1f, 0.1f, 0.0f);
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            Gl.glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MyPaint(object sender, PaintEventArgs e)
+        {
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+            Gl.glColor3f(0.2f, 0.5f, 0.0f);
+            //Gl.glTranslatef(x, y, z);
+            
+            Gl.glPushMatrix();
+            Gl.glTranslatef(0.5f, 0.5f, 0);
+            Gl.glRotatef(alfa, 0, 0, 1);
+            Gl.glTranslatef(-0.5f, -0.5f, 0);
+            Gl.glBegin(Gl.GL_TRIANGLES);
+                    Gl.glVertex3f(0.0f, 0.0f, 0.0f);
+                    Gl.glVertex3f(0.5f, 0.5f, 0.0f);
+                    Gl.glVertex3f(1.0f, 0.0f, 0.0f);
+                    Gl.glVertex3f(0.0f, 1.0f, 0.0f);
+                    Gl.glVertex3f(0.5f, 0.5f, 0.0f);
+                    Gl.glVertex3f(1.0f, 1.0f, 0.0f);
+            Gl.glEnd();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            alfa = (alfa + 5) % 360;
+            OpenGlControl.Refresh();
+        }
+
+        private void MyKeys(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+                alfa = (alfa + 5) % 360;
+            if (e.KeyCode == Keys.D)
+                alfa = (alfa - 5) % 360;
+            OpenGlControl.Refresh();
+        }
+    }
+}
